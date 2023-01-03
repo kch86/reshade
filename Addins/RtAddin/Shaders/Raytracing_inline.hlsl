862da1d6d977ff423197f885a7439f56c1faf2c4
kch_86 <kch_86@yahoo.com>
2023-01-03 09:58:02 -0500

    add example inlinen trace

--- Addins/RtAddin/Shaders/Raytracing_inline.hlsl
+++ Addins/RtAddin/Shaders/Raytracing_inline.hlsl
@@ -61,9 +61,64 @@ RWTexture2D<float4> RenderTarget : register(u0);
 void ray_gen(uint3 tid : SV_GroupThreadID)
 {
     //trace
+#if 0
+	 // a. Configure
+	RayQuery<RAY_FLAG_CULL_NON_OPAQUE | RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> query;
+
+	uint ray_flags = 0; // Any this ray requires in addition those above.
+	uint ray_instance_mask = 0xffffffff;
+
+	// b. Initialize  - hardwired here to deliver minimal sample code.
+	RayDesc ray;
+	ray.TMin = 1e-5f;
+	ray.TMax = 1e10f;
+	ray.Origin = float3(0, 0, 0);
+	ray.Direction = float3(0, 0, 1);
+	query.TraceRayInline(Scene, ray_flags, ray_instance_mask, ray);
+
+	// c. Cast 
+
+	// Proceed() is where behind-the-scenes traversal happens, including the heaviest of any driver inlined code.
+	// In this simplest of scenarios, Proceed() only needs to be called once rather than a loop.
+	// Based on the template specialization above, traversal completion is guaranteed.
+
+	float4 value = 0.0;
+	while (query.Proceed())
+	{
+		// d. Examine and act on the result of the traversal.
+
+		if (query.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
+		{
+			// TODO: Grab ray parameters & sample accordingly.
+
+			/* ShadeMyTriangleHit(
+				query.CommittedInstanceIndex(),
+				query.CommittedPrimitiveIndex(),
+				query.CommittedGeometryIndex(),
+				query.CommittedRayT(),
+				query.CommittedTriangleBarycentrics(),
+				query.CommittedTriangleFrontFace() );*/
+
+			value = float4(0, 1, 0, 1);
+			break;
+		}
+		else
+		{
+			// COMMITTED_NOTHING. From template specialization above, COMMITTED_PROCEDURAL_PRIMITIVE can't happen so no need to check for that.
+
+			// Miss shading - sample the environment.
+			// Environment_Sample(query.WorldRayOrigin(), query.WorldRayDirection()); 
+
+			value = float4(0, 0, 1, 1);
+		}
+	}
+#else
+	float4 value = float4(1.0, 0.2, 0.2, 1.0);
+#endif
 
     // Write the raytraced color to the output texture.
-    RenderTarget[tid.xy] = float4(1.0, 0.2, 0.2, 1.0);
+    //RenderTarget[tid.xy] = float4(1.0, 0.2, 0.2, 1.0);
+	RenderTarget[tid.xy] = value;
 }
 
 
