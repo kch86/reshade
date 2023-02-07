0df3c8c363750c5ff2a4701ddcf3f3f12821b617
kch_86 <kch_86@yahoo.com>
2023-02-07 08:28:49 +0100

    fix hlsl 2021 select issue

--- Addins/RtAddin/Shaders/Color.h
+++ Addins/RtAddin/Shaders/Color.h
@@ -3,12 +3,14 @@
 
 float3 to_srgb_from_linear(float3 x)
 {
-	return x < 0.0031308 ? 12.92 * x : 1.055 * pow(x, 1.0 / 2.4) - 0.055;
+	//return x < 0.0031308 ? 12.92 * x : 1.055 * pow(x, 1.0 / 2.4) - 0.055;
+	return select(x < 0.0031308, 12.92 * x, 1.055 * pow(x, 1.0 / 2.4) - 0.055);
 }
 
 float3 to_linear_from_srgb(float3 x)
 {
-	return x < 0.04045 ? x / 12.92 : pow((x + 0.055) / 1.055, 2.4);
+	//return x < 0.04045 ? x / 12.92 : pow((x + 0.055) / 1.055, 2.4);
+	return select(x < 0.04045, x / 12.92, pow((x + 0.055) / 1.055, 2.4));
 }
 
 float get_luminance(float3 c)
