2c1d8c05a86e46b43ce6fb319f4104abcdd4fe51
kch_86 <kch_86@yahoo.com>
2023-03-13 09:17:32 +0100

    unique naes for tracy

--- Addins/RtAddin/profiling.h
+++ Addins/RtAddin/profiling.h
@@ -3,11 +3,15 @@
 #include <superluminal/PerformanceAPI.h>
 #include <tracy/public/tracy/Tracy.hpp>
 
-#define USE_TRACY 0
+#define USE_TRACY 1
 #define USE_SL 1
 
 namespace profiling
 {
+#define PROFILE_CAT_IMPL(a, b) a##b	
+#define PROFILE_CAT(a, b) PROFILE_CAT_IMPL(a, b)	
+#define PROFILE_UNIQUE_IDENTIFIER(a) PROFILE_CAT(a, __LINE__)
+
 #define PROFILE_BEGIN(name) PerformanceAPI_BeginEvent(name, nullptr, PERFORMANCEAPI_DEFAULT_COLOR)
 #define PROFILE_BEGIN_DATA(name, data) PerformanceAPI_BeginEvent(name, data, PERFORMANCEAPI_DEFAULT_COLOR)
 #define PROFILE_END() PerformanceAPI_EndEvent()
@@ -23,7 +27,7 @@ namespace profiling
 #endif
 
 #if USE_TRACY
-#define TRACY_SCOPE(name) ZoneScopedN(name)
+#define TRACY_SCOPE(name) ZoneNamedN(PROFILE_UNIQUE_IDENTIFIER(_profiler_var_), name, true)
 #define TRACY_FRAME_END FrameMark
 #else
 #define TRACY_SCOPE(name)
